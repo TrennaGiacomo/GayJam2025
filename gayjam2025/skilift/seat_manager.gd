bfc8db0fbf9175672cf22d255fe7610c395c0186
oguzhan00yildiz <oguzhan00yildiz@gmail.com>
2025-01-13 16:21:11 +0200

    Seats made

---
+++ gayjam2025/skilift/seat_manager.gd
@@ -0,0 +1,35 @@
+extends Node2D
+@export_file var seatpath
+
+@onready var seatScene=load(seatpath) as PackedScene
+
+var spacing = 50 # Spacing between sprites
+var layout_mode = "horizontal"  # Options: "horizontal" or "vertical"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	MakeSeats(3)
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func MakeSeats(seatCount:int):
+	while seatCount >0:
+		var seatInstace = seatScene.instantiate() as seat
+		add_child(seatInstace)
+		seatInstace.name="seat"
+		seatCount-=1
+	arrange_sprites()
+		
+func arrange_sprites():
+	var offset = Vector2.ZERO
+	for child in get_children():
+		if child is Sprite2D:
+			child.position = offset
+			if layout_mode == "horizontal":
+				offset.x += child.texture.get_width() + spacing
+			elif layout_mode == "vertical":
+				offset.y += child.texture.get_height() + spacing
